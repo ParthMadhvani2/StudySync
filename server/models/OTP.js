9733e7b13a8796fbb09fe73c611fe701e5fbaea3
Parth Madhvani <madhvaniparth2@gmail.com>
2024-03-10 00:55:22 +0530

    models created

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -16,4 +16,22 @@ const OTPSchema = new mongoose.Schema({
     },
 });
 
+// a function -> to send emails
+async function sendVerificationEmail(email, otp){
+    try{
+        const mailResponse = await mailSender(email, "Verification Email from StudyNotion",otp);
+        console.log("Email send successfully:", mailResponse);
+    }
+    catch(error){
+        console.log("error has occured while sending mails with otp",error);
+        throw error;
+
+    }
+}
+
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+})
+
 module.exports = mongoose.model("OTP",OTPSchema);
