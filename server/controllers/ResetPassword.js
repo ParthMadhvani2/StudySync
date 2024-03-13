40f3445aec8827aa2c15c63607471c50f6f433eb
Parth Madhvani <madhvaniparth2@gmail.com>
2024-03-13 21:37:39 +0530

    reset password

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -1,7 +1,24 @@
-
+const User = require("../models/User");
+const mailSender = require("../utlis/mailSender");
 
 //reset Password token
-
+exports.resetPasswordToken = async (req,res) => {
+    // get req from user body
+    const email = req.body.email;
+    // check user for this email, email validation
+    const user = await User.findOne({email:email});
+    if(!user){
+        return res.json({
+            success:false,
+            message:"User not registered with us"
+        });
+    }
+    // generate token
+    // update to user by adding token and expiration time
+    // create url
+    // send mail containing url
+    // return response
+}
 
 
 //reset Password
