47ecaae1175bc6a38b7456116b912a35f8554f7e
Parth Madhvani <madhvaniparth2@gmail.com>
2024-05-05 21:41:54 +0530

    review&rating controller

---
+++ server/controllers/RatingAndReview.js
@@ -0,0 +1,20 @@
+const RatingAndReview = require("../models/RatingAndReview");
+const course = require("../models/Course");
+
+//creatingRating
+exports.creatingRating = async(req,res) => {
+    try{
+        //get user id
+        //fetch data from request body
+        //check if user is enrolled or not
+        //check if user already reviewed the course
+        //create rating and review
+        
+    }
+    catch(error){}
+};
+
+
+// getAverageRating
+
+//getAllRating
