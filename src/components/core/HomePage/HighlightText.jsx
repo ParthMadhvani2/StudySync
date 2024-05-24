450ad05efb6a06e6aec1c5aeca128afda5c072b9
Parth Madhvani <madhvaniparth2@gmail.com>
2024-05-24 18:38:05 +0530

    home page UI

--- src/components/core/HomePage/HighlightText.jsx
+++ src/components/core/HomePage/HighlightText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HighlightText = ({text}) => {
   return (
-    <span className='font-bold bg-clip-text text-transparent bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB]'>
+    <span className='font-bold text-richblue-200'>
         {" "}
         {text}
     </span>
