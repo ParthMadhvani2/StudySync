9f5a765d84ba811e6217e567902c6a97a3aa1bfc
Parth Madhvani <madhvaniparth2@gmail.com>
2024-06-06 23:04:03 +0530

    navbar UI

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, matchPath } from 'react-router-dom'
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import {NavbarLinks} from "../../data/navbar-links";
-
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+    const location = useLocation();
+    const matchRoute = (route) => {
+        return matchPath({path:route}, location.pathname);
+    }
   return (
     <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
         <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
@@ -17,18 +21,27 @@ const Navbar = () => {
             <nav>
                 <ul className='flex gap-x-6 text-richblack-25'>
                     {
-                        NavbarLinks.map( (link,index) => {
+                        NavbarLinks.map( (link,index) => (
                             <li key={index}>
                                 {
                                     link.title === "Catalog" ? (<div></div>) : (
-                                        <Link></Link>
+                                        <Link to={link?.path}>
+                                            <p className={`${matchRoute(link?.path)? "text-yellow-25" : "text-richblack-25"}`}>
+                                                {link.title}
+                                            </p>
+                                        </Link>
                                     )
                                 }
                             </li>
-                        } )
+                         ) )
                     }
                 </ul>
             </nav>
+
+            {/* login/Dignup/Dashboard */}
+            <div className='flex gap-x-4 items-center'>
+
+            </div>
         </div>
     </div>
   )
