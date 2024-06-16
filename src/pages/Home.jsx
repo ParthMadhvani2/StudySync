3cd73f4c516088920f35c9e9f0ff36aedd68f866
Parth Madhvani <madhvaniparth2@gmail.com>
2024-06-16 23:50:39 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,11 @@ import HighlightText from '../components/core/HomePage/HighlightText'
 import CTAButton from "../components/core/HomePage/Button"
 import Banner from "../assets/Images/banner.mp4"
 import CodeBlocks from "../components/core/HomePage/CodeBlocks"
-import Footer from '../components/common/Footer'
-import ExploreMore from '../components/core/HomePage/ExploreMore'
 import TimelineSection from '../components/core/HomePage/TimelineSection'
 import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
 import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from '../components/common/Footer'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
 
 const Home = () => {
   return (
@@ -21,7 +21,7 @@ const Home = () => {
 
         <Link to={"/signup"}>
             <div className=' group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
-            transition-all duration-200 hover:scale-95 w-fit shadow-sm shadow-richblack-200'>
+            transition-all duration-200 hover:scale-95 w-fit'>
                 <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
                 transition-all duration-200 group-hover:bg-richblack-900'>
                     <p>Become an Instructor</p>
@@ -67,7 +67,7 @@ const Home = () => {
                 heading={
                     <div className='text-4xl font-semibold'>
                         Unlock Your
-                        <HighlightText text={"coding potential "}/>
+                        <HighlightText text={"coding potential"}/>
                         with our online courses
                     </div>
                 }
@@ -100,12 +100,13 @@ const Home = () => {
                 position={"lg:flex-row-reverse"}
                 heading={
                     <div className='text-4xl font-semibold'>
-                        Start
-                        <HighlightText text={"coding in seconds "}/>
+                        Unlock Your
+                        <HighlightText text={"coding potential"}/>
+                        with our online courses
                     </div>
                 }
                 subheading = {
-                    "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+                    "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
                 }
                 ctabtn1={
                     {
@@ -130,76 +131,78 @@ const Home = () => {
             <ExploreMore />
       </div>
 
-        {/* Section 2 */}
-            <div className='bg-pure-greys-5 text-richblack-700'>
-                <div className='homepage_bg h-[310px]'>
+      {/*Section 2  */}
+      <div className='bg-pure-greys-5 text-richblack-700'>
+            <div className='homepage_bg h-[310px]'>
 
-                    <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
-                        <div className='h-[150px]'></div>
-                        <div className='flex flex-row gap-7 text-white '>
-                            <CTAButton active={true} linkto={"/signup"}>
-                                <div className='flex items-center gap-3' >
-                                    Explore Full Catalog
-                                    <FaArrowRight />
-                                </div>
-                                
-                            </CTAButton>
-                            <CTAButton active={false} linkto={"/signup"}>
-                                <div>
-                                    Learn more
-                                </div>
-                            </CTAButton>
-                        </div>
-
-                    </div>
-
-
-                </div>
-
-                <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
-
-                    <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
-                        <div className='text-4xl font-semibold w-[45%]'>
-                            Get the Skills you need for a
-                            <HighlightText text={"Job that is in demand"} />
-                        </div>
-
-                        <div className='flex flex-col gap-10 w-[40%] items-start'>
-                        <div className='text-[16px]'>
-                        The modern StudySync is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.
-                        </div>
+                <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+                    <div className='h-[150px]'></div>
+                    <div className='flex flex-row gap-7 text-white '>
                         <CTAButton active={true} linkto={"/signup"}>
+                            <div className='flex items-center gap-3' >
+                                Explore Full Catalog
+                                <FaArrowRight />
+                            </div>
+                            
+                        </CTAButton>
+                        <CTAButton active={false} linkto={"/signup"}>
                             <div>
                                 Learn more
                             </div>
                         </CTAButton>
-                        </div>
-
                     </div>
-                    
-                    
-
-                    <TimelineSection />
-
-                    <LearningLanguageSection />
 
                 </div>
 
+
+            </div>
+
+            <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
+
+                <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+                    <div className='text-4xl font-semibold w-[45%]'>
+                        Get the Skills you need for a
+                        <HighlightText text={"Job that is in demand"} />
+                    </div>
+
+                    <div className='flex flex-col gap-10 w-[40%] items-start'>
+                    <div className='text-[16px]'>
+                    The modern StudySync is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.
+                    </div>
+                    <CTAButton active={true} linkto={"/signup"}>
+                        <div>
+                            Learn more
+                        </div>
+                    </CTAButton>
+                    </div>
+
+                </div>
+                
                 
 
-        </div>
-        {/* Section 3 */}
-        <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+                <TimelineSection />
+
+                <LearningLanguageSection />
+
+            </div>
+
+            
+
+      </div>
+
+
+      {/*Section 3 */}
+      <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
 
             <InstructorSection />
 
             <h2 className='text-center text-4xl font-semobold mt-10'>review from Other Learners</h2>
             {/* Review Slider here */}
-        </div>
+      </div>
 
 
-        {/*Footer */}
-            <Footer />
+      {/*Footer */}
+      <Footer />
 
     </div>
   )
