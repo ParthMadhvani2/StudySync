e19590bb3423d5d012278ed7b2f1bbbd33572173
Parth Madhvani <madhvaniparth2@gmail.com>
2024-06-16 23:30:32 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import {Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
-import Navbar from "./components/common/Navbar";
+import Navbar from "./components/common/Navbar"
 import OpenRoute from "./components/core/Auth/OpenRoute"
 
 import Login from "./pages/Login"
@@ -13,22 +13,22 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route 
-              path="/signup"
-              element={
-                <OpenRoute>
-                  <Signup/>
-                </OpenRoute>
-              }
-      />
-      <Route 
-              path="/login"
-              element={
-                <OpenRoute>
-                  <Login/>
-                </OpenRoute>
-              }
-      />
+      <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+    <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
     </Routes>
    </div>
   );
